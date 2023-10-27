09dbcad1d22521f23da95b587db7c8b2e9c5d711
CodeWebSpirit <147144389+CodeWebSpirit@users.noreply.github.com>
2023-10-27 11:21:47 +0200

    Add files via upload

---
+++ Osoba i adres/Adres.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Adres
+    {
+        public string Ulica {  get; set; }
+        public string NumerDomu { get; set;}
+        public string NumerMieszkania { get; set; }
+        public string KodPocztowy { get; set; }
+        public string Miasto {  get; set; }
+        public string Państwo {  get; set; }
+
+        public string AdresPocztowy
+        {
+            get
+            {
+                return $"ul. {Ulica} {NumerDomu}/{NumerMieszkania}\n{KodPocztowy} {Miasto}\n{Państwo}";
+            }
+        }
+    }
+}
---
+++ Osoba i adres/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Xml.Schema;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Osoba osoba = new Osoba();
+            osoba.Imie = "Bartosz";
+            osoba.Nazwisko = "Cieżuch";
+            Adres CieżuchAdres = new Adres()
+            {
+                Ulica = "Zdrój",
+                NumerDomu = "4",
+                NumerMieszkania = "A",
+                KodPocztowy = "62-065",
+                Miasto = "Grodzisk Wielkopolski",
+                Państwo = "Polska",
+            };*/
+            Console.WriteLine("Podaj liczbe kontaktów: ");
+            int liczbakontaktów = int.Parse(Console.ReadLine());
+            Osoba[] ludzie = new Osoba[liczbakontaktów];
+            for(int i = 0; i < liczbakontaktów; i++)
+            {
+                Console.WriteLine($"Podaj imię kontaktu #{i + 1}:");
+                string imie = Console.ReadLine();
+                Console.WriteLine($"Podaj nazwisko kontaktu #{i + 1}:");
+                string nazwisko = Console.ReadLine();
+
+                ludzie[i] = new Osoba();
+
+                Console.WriteLine($"Podaj ulicę kontaktu #{i + 1}:");
+                string Ulica = Console.ReadLine();
+
+                Console.WriteLine($"Podaj numer domu kontaktu #{i + 1}:");
+                string NumerDomu = Console.ReadLine();
+            }
+        }
+        
+            
+               
+        
+            
+        
+        
+       
+            
+        
+
+    }
+}
