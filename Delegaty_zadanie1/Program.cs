dfca6ba8484986e7a4efca08690877a8a7c4d256
CodeWebSpirit <147144389+CodeWebSpirit@users.noreply.github.com>
2023-12-08 11:15:33 +0100

    Add files via upload

---
+++ Delegaty_zadanie1/Program.cs
@@ -0,0 +1,46 @@
+﻿using static Delegaty_zadanie1.Program;
+
+namespace Delegaty_zadanie1
+{
+    internal class Program
+    {
+        public delegate int operacja(int x,int y);
+        public static int dodaj(int x, int y)
+        {
+            return x + y;
+        }
+        public static int odejmij(int x, int y)
+        {
+            return x - y;
+        }
+        public static int pomnóż(int x, int y)
+        {
+            return x * y;
+        }
+        public static int podziel(int x, int y)
+        {
+            return x / y;
+        }
+        static void Main(string[] args)
+        {
+            operacja dodawanie = new operacja(dodaj);
+            operacja odejmowanie = new operacja(odejmij);
+            operacja mnożenie = new operacja(pomnóż);
+            operacja dzielenie = new operacja(podziel);
+
+        }
+        public static void WyświetlWyniki(operacja op, int x, int y)
+        {
+            int wynik;
+            if (op.Method.Name == "podziel" && y ==0)
+            {
+                Console.WriteLine("dzielenie przez zero");
+                wynik = 0;
+            }
+            else {
+                wynik = op(x, y);
+                Console.WriteLine("wynik operacji na liczbach wynosi", op.Method.Name)
+            }
+        }
+    }
+}
