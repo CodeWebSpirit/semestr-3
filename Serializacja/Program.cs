a508d487d37d7f0df146b42b639f08fcedd13334
CodeWebSpirit <147144389+CodeWebSpirit@users.noreply.github.com>
2024-01-12 11:11:24 +0100

    Add files via upload

---
+++ Serializacja/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.Design.Serialization;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace serializacja2
+{
+    public class Program
+    {
+        public class Person : IXmlSerializable
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+
+            public void WriteXml(XmlWriter writer)
+            {
+                writer.WriteAttributeString("FirstName", FirstName);
+                writer.WriteAttributeString("LastName", LastName);
+                writer.WriteAttributeString("Age", Age.ToString());
+            }
+
+            public void ReadXml(XmlReader reader)
+            {
+                reader.MoveToContent();
+                FirstName = reader.GetAttribute("FirstName");
+                LastName = reader.GetAttribute("LastName");
+                Age = int.Parse(reader.GetAttribute("Age"));
+            }
+
+            public System.Xml.Schema.XmlSchema GetSchema()
+            {
+                return null;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Person p = new Person { FirstName = "Franek", LastName = "Kowalski", Age = 20 };
+            XmlSerializer serializer = new XmlSerializer(typeof(Person));
+            using (FileStream s = File.Create("C:\\Users\\Student\\source\\repos\\Serializacja\\osoba.xml"))
+            {
+                serializer.Serialize(s, p);
+                //s.Close();
+            }
+
+            using (FileStream s = File.OpenRead("C:\\Users\\Student\\source\\repos\\Serializacja\\osoba.xml"))
+            {
+                Person p2 = (Person)serializer.Deserialize(s);
+                //s.Close();
+                Console.WriteLine("Imię: {0}, nazwisko: {1}, wiek: {2}", p2.FirstName, p2.LastName, p2.Age);
+            }
+        }
+    }
+}
